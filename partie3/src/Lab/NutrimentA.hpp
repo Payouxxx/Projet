d48fa1dca5a446fa4e502815708d5923d3a14309
Crozat Florence Natacha Sabine <crozat@INTRANET.EPFL.CH@IC-IN-SC-36.intranet.epfl.ch>
2020-04-06 09:56:11 +0200

    commentaires nutriments et petri dish

--- partie3/src/Lab/NutrimentA.hpp
+++ partie3/src/Lab/NutrimentA.hpp
@@ -3,10 +3,25 @@
 
 #include "Nutriment.hpp"
 
+
+/*!
+ * @brief the NutrimentA class modeling a green nutriment
+ */
+
 class NutrimentA : public Nutriment
 {
 public:
+    /*!
+     * @brief constructor
+     * @param rayon
+     * @param centre
+     */
     NutrimentA(Quantity rayon, Vec2d centre);
+
+    /*!
+     * @brief shortcut to a location in the file app.json
+     * @return shortcut
+     */
     j::Value const& getConfig() const;
 };
 
--- partie3/src/Lab/NutrimentB.hpp
+++ partie3/src/Lab/NutrimentB.hpp
@@ -3,10 +3,25 @@
 
 #include "Nutriment.hpp"
 
+
+/*!
+ * @brief the NutrimentB class modeling a green nutriment
+ */
+
 class NutrimentB : public Nutriment
 {
 public:
+    /*!
+     * @brief constructor
+     * @param rayon
+     * @param centre
+     */
     NutrimentB(Quantity rayon, Vec2d centre);
+
+    /*!
+     * @brief shortcut to a location in the file app.json
+     * @return shortcut
+     */
     j::Value const& getConfig() const;
 };
 
